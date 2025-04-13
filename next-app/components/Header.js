d0ec67876b76178765375fa776951d12bd633a63
john.lombardo <john.lombardo@gmail.com>
2025-04-13 23:46:27 +0000

    More menu fixes

--- next-app/components/Header.js
+++ next-app/components/Header.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Header() {
   return (
@@ -15,19 +16,19 @@ export default function Header() {
               </a>
             </Link>
           </div>
-          <div className="d-flex align-items-center w-100 justify-content-end">
+          <div className="d-flex align-items-center justify-content-end">
             <button
-              className="navbar-toggler"
+              className="navbar-toggler collapsed"
               type="button"
               data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
+              data-bs-target="#mobileMenu"
               aria-controls="navbarNav"
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse w-100" id="navbarNav">
+            <div className="collapse navbar-collapse" id="navbarNav">
               <div className="navbar-nav w-100 justify-content-end">
                 <Link href="/" legacyBehavior><a className="nav-link mx-3">Home</a></Link>
                 <Link href="/calculator" legacyBehavior><a className="nav-link mx-3">Chip Calculator</a></Link>
@@ -35,10 +36,19 @@ export default function Header() {
                 <Link href="/payout-calculator" legacyBehavior><a className="nav-link mx-3">Payout Calculator</a></Link>
                 <Link href="/about" legacyBehavior><a className="nav-link mx-3">About</a></Link>
               </div>
-                    </div>
-                </div>
             </div>
+          </div>
         </div>
+        <div className="collapse mobile-menu" id="mobileMenu">
+          <div className="navbar-nav w-100 flex-column">
+            <Link href="/" legacyBehavior><a className="nav-link">Home</a></Link>
+            <Link href="/calculator" legacyBehavior><a className="nav-link">Chip Calculator</a></Link>
+            <Link href="/blind-structure" legacyBehavior><a className="nav-link">Blind Structure</a></Link>
+            <Link href="/payout-calculator" legacyBehavior><a className="nav-link">Payout Calculator</a></Link>
+            <Link href="/about" legacyBehavior><a className="nav-link">About</a></Link>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 }
